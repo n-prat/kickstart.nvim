951cf2f56d59fe4b267937b9b060fbeec4abd598
n-prat <1504071+n-prat@users.noreply.github.com>
2024-12-21 18:30:26 +0100

    add plugin: persisted\nSession manager

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,19 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+pcall(require('telescope').load_extension, 'persisted')
+return {
+  -- https://github.com/olimorris/persisted.nvim
+  -- NOTE "By design, the plugin will not autoload a session when any arguments are passed to Neovim" eg `nvim .`
+  {
+    'olimorris/persisted.nvim',
+    config = function()
+      require('persisted').setup {
+        autoload = true,
+        on_autoload_no_session = function()
+          vim.notify 'No existing session to load.'
+        end,
+      }
+    end,
+  },
+}
