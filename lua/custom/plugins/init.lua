6751dd5ae5328663119e599cab3bce30f681c97b
n-prat <1504071+n-prat@users.noreply.github.com>
2024-12-21 20:03:53 +0100

    add plugin: neogit

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -17,4 +17,21 @@ return {
       }
     end,
   },
+
+  -----------------------------------------------------------------------------
+  --- https://github.com/NeogitOrg/neogit
+  --- ALTERNATIVE: fugitive, but bigger/slower and older?
+  {
+    'NeogitOrg/neogit',
+    dependencies = {
+      'nvim-lua/plenary.nvim', -- required
+      'sindrets/diffview.nvim', -- optional - Diff integration
+
+      -- Only one of these is needed.
+      'nvim-telescope/telescope.nvim', -- optional
+      -- 'ibhagwan/fzf-lua', -- optional
+      -- 'echasnovski/mini.pick', -- optional
+    },
+    config = true,
+  },
 }
