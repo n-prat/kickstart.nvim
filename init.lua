92ed146b4b1f93c4be46453e810b80fb5c808847
n-prat <1504071+n-prat@users.noreply.github.com>
2024-12-28 12:01:24 +0100

    add GUI support: Neovide
    With all animations disabled

--- init.lua
+++ init.lua
@@ -1000,6 +1000,7 @@ require('lazy').setup({
 })
 
 require 'custom.n-prat.remap'
+require 'custom.n-prat.neovide'
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
---
+++ lua/custom/n-prat/neovide.lua
@@ -0,0 +1,12 @@
+if vim.g.neovide then
+  -- Put anything you want to happen only in Neovide here
+  -- https://neovide.dev/configuration.html
+  vim.g.neovide_scroll_animation_length = 0.1
+  vim.g.neovide_scroll_animation_far_lines = 0
+  vim.g.neovide_cursor_animation_length = 0
+  vim.g.neovide_cursor_trail_size = 0
+  vim.g.neovide_cursor_animate_in_insert_mode = false
+  vim.g.neovide_cursor_animate_command_line = false
+  vim.g.neovide_cursor_smooth_blink = false
+  vim.g.neovide_cursor_vfx_mode = ''
+end
