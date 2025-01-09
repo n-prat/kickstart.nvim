8adc88f00aab1ee7322fe2cbc02d4e80c01ad3da
n-prat <1504071+n-prat@users.noreply.github.com>
2025-01-09 11:55:51 +0100

    custom: add comment about build-and-run loop

---
+++ lua/custom/plugins/build_and_run.lua
@@ -0,0 +1,5 @@
+-- For now, just use eg `cargo watch`(now `cargo bacon`) in a basic standard Terminal split
+-- MAYBE later use https://github.com/stevearc/overseer.nvim and/or https://github.com/Zeioth/compiler.nvim
+-- and/or roll your own command using eg BufPostWrite cf https://youtu.be/9gUatBHuXE0?si=qhL9amKgUkgbjCUa and https://youtu.be/HlfjpstqXwE?si=g84pU1MBOBX5n5Qd
+
+return {}
