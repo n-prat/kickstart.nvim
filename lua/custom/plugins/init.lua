2453e5b5436b795426900a374696e3dda82de7d4
n-prat <1504071+n-prat@users.noreply.github.com>
2026-01-09 16:33:04 +0100

    backport more TS related keybinds etc
    - enable precognition by default

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -790,8 +790,8 @@ return {
   },
   -------------------------------------------------------------------------------
   --- https://github.com/nvim-treesitter/nvim-treesitter-textobjects
-  --- TODO see https://github.com/LazyVim/LazyVim/blob/d0c366e4d861b848bdc710696d5311dca2c6d540/lua/lazyvim/plugins/treesitter.lua#L76
-  --- for alternative keymaps
+  --- Simplified to match nvim-nightly 0.12 config (only af/if textobjects)
+  --- Removed: ac/ic, as, swap, move, ts_repeat_move
   {
     'nvim-treesitter/nvim-treesitter-textobjects',
     event = 'VeryLazy',
@@ -803,163 +803,58 @@ return {
         textobjects = {
           select = {
             enable = true,
-
-            -- Automatically jump forward to textobj, similar to targets.vim
             lookahead = true,
-
             keymaps = {
-              -- You can use the capture groups defined in textobjects.scm
+              -- Only af/if to match nvim-nightly 0.12
               ['af'] = '@function.outer',
               ['if'] = '@function.inner',
-              ['ac'] = '@class.outer',
-              -- You can optionally set descriptions to the mappings (used in the desc parameter of
-              -- nvim_buf_set_keymap) which plugins like which-key display
-              ['ic'] = { query = '@class.inner', desc = 'Select inner part of a class region' },
-              -- You can also use captures from other query groups like `locals.scm`
-              ['as'] = { query = '@local.scope', query_group = 'locals', desc = 'Select language scope' },
             },
-            -- You can choose the select mode (default is charwise 'v')
-            --
-            -- Can also be a function which gets passed a table with the keys
-            -- * query_string: eg '@function.inner'
-            -- * method: eg 'v' or 'o'
-            -- and should return the mode ('v', 'V', or '<c-v>') or a table
-            -- mapping query_strings to modes.
             selection_modes = {
-              ['@parameter.outer'] = 'v', -- charwise
               ['@function.outer'] = 'V', -- linewise
-              ['@class.outer'] = '<c-v>', -- blockwise
             },
-            -- If you set this to `true` (default is `false`) then any textobject is
-            -- extended to include preceding or succeeding whitespace. Succeeding
-            -- whitespace has priority in order to act similarly to eg the built-in
-            -- `ap`.
-            --
-            -- Can also be a function which gets passed a table with the keys
-            -- * query_string: eg '@function.inner'
-            -- * selection_mode: eg 'v'
-            -- and should return true or false
             include_surrounding_whitespace = true,
           },
         },
-
-        swap = {
-          enable = true,
-          swap_next = {
-            ['<leader>a'] = '@parameter.inner',
-          },
-          swap_previous = {
-            ['<leader>A'] = '@parameter.inner',
-          },
-        },
-
-        move = {
-          enable = true,
-          set_jumps = true, -- whether to set jumps in the jumplist
-          -----------------------------------------------------------
-          --- Official README bindings
-          goto_next_start = {
-            [']m'] = '@function.outer',
-            [']]'] = { query = '@class.outer', desc = 'Next class start' },
-            --
-            -- You can use regex matching (i.e. lua pattern) and/or pass a list in a "query" key to group multiple queries.
-            [']o'] = '@loop.*',
-            -- ["]o"] = { query = { "@loop.inner", "@loop.outer" } }
-            --
-            -- You can pass a query group to use query from `queries/<lang>/<query_group>.scm file in your runtime path.
-            -- Below example nvim-treesitter's `locals.scm` and `folds.scm`. They also provide highlights.scm and indent.scm.
-            [']s'] = { query = '@local.scope', query_group = 'locals', desc = 'Next scope' },
-            [']z'] = { query = '@fold', query_group = 'folds', desc = 'Next fold' },
-          },
-          goto_next_end = {
-            [']M'] = '@function.outer',
-            [']['] = '@class.outer',
-          },
-          goto_previous_start = {
-            ['[m'] = '@function.outer',
-            ['[['] = '@class.outer',
-          },
-          goto_previous_end = {
-            ['[M'] = '@function.outer',
-            ['[]'] = '@class.outer',
-          },
-          -----------------------------------------------------------
-          --- LazyVim bindings
-          -- goto_next_start = {
-          --   [']f'] = '@function.outer',
-          --   [']c'] = '@class.outer',
-          --   --
-          --   [']a'] = '@parameter.inner',
-          -- },
-          -- goto_next_end = { --
-          --   [']F'] = '@function.outer',
-          --   [']C'] = '@class.outer',
-          --   [']A'] = '@parameter.inner',
-          -- },
-          -- goto_previous_start = { --
-          --   ['[f'] = '@function.outer',
-          --   ['[c'] = '@class.outer',
-          --   ['[a'] = '@parameter.inner',
-          -- },
-          -- goto_previous_end = { --
-          --   ['[F'] = '@function.outer',
-          --   ['[C'] = '@class.outer',
-          --   ['[A'] = '@parameter.inner',
-          -- },
-          -----------------------------------------------------------
-          -- Below will go to either the start or the end, whichever is closer.
-          -- Use if you want more granular movements
-          -- Make it even more gradual by adding multiple queries and regex.
-          goto_next = {
-            [']d'] = '@conditional.outer',
-          },
-          goto_previous = {
-            ['[d'] = '@conditional.outer',
-          },
-        },
+        -- swap/move removed to match nvim-nightly 0.12
       }
 
-      local ts_repeat_move = require 'nvim-treesitter.textobjects.repeatable_move'
+      -- Incremental selection (matches nvim-nightly 0.12)
+      vim.keymap.set('n', '<C-space>', function()
+        require('nvim-treesitter.incremental_selection').init_selection()
+      end, { desc = 'Treesitter: init selection' })
+      vim.keymap.set('x', '<C-space>', function()
+        require('nvim-treesitter.incremental_selection').node_incremental()
+      end, { desc = 'Treesitter: increment selection' })
+      vim.keymap.set('x', '<bs>', function()
+        require('nvim-treesitter.incremental_selection').node_decremental()
+      end, { desc = 'Treesitter: decrement selection' })
 
-      -- Repeat movement with ; and ,
-      -- ensure ; goes forward and , goes backward regardless of the last direction
-      vim.keymap.set({ 'n', 'x', 'o' }, ';', ts_repeat_move.repeat_last_move_next)
-      vim.keymap.set({ 'n', 'x', 'o' }, ',', ts_repeat_move.repeat_last_move_previous)
-
-      -- vim way: ; goes to the direction you were moving.
-      -- vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move)
-      -- vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_opposite)
-
-      -- Optionally, make builtin f, F, t, T also repeatable with ; and ,
-      vim.keymap.set({ 'n', 'x', 'o' }, 'f', ts_repeat_move.builtin_f_expr, { expr = true })
-      vim.keymap.set({ 'n', 'x', 'o' }, 'F', ts_repeat_move.builtin_F_expr, { expr = true })
-      vim.keymap.set({ 'n', 'x', 'o' }, 't', ts_repeat_move.builtin_t_expr, { expr = true })
-      vim.keymap.set({ 'n', 'x', 'o' }, 'T', ts_repeat_move.builtin_T_expr, { expr = true })
+      -- NOTE: ts_repeat_move removed - using vanilla ;/, for f/t repeat
     end,
   },
   -------------------------------------------------------------------------------
   --- https://github.com/RRethy/nvim-treesitter-textsubjects
-  --- Basically a simplified textobjects + move
-  {
-    'RRethy/nvim-treesitter-textsubjects',
-    event = 'VeryLazy',
-    -- MUST use same cond than treesitter itself; or could be cleverer about loading cf linked lazyvim config
-    cond = not jit.os:find 'Windows',
-    dependencies = { 'nvim-treesitter/nvim-treesitter' },
-    config = function()
-      require('nvim-treesitter.configs').setup {
-        textsubjects = {
-          enable = true,
-          prev_selection = ',', -- (Optional) keymap to select the previous selection
-          keymaps = {
-            ['.'] = 'textsubjects-smart',
-            [';'] = 'textsubjects-container-outer',
-            ['i;'] = { 'textsubjects-container-inner', desc = 'Select inside containers (classes, functions, etc.)' },
-          },
-        },
-      }
-    end,
-  },
+  --- DISABLED: Not compatible with treesitter main branch (used in nvim-nightly 0.12)
+  --- Keeping disabled for parity with nvim-nightly config
+  -- {
+  --   'RRethy/nvim-treesitter-textsubjects',
+  --   event = 'VeryLazy',
+  --   cond = not jit.os:find 'Windows',
+  --   dependencies = { 'nvim-treesitter/nvim-treesitter' },
+  --   config = function()
+  --     require('nvim-treesitter.configs').setup {
+  --       textsubjects = {
+  --         enable = true,
+  --         prev_selection = ',',
+  --         keymaps = {
+  --           ['.'] = 'textsubjects-smart',
+  --           [';'] = 'textsubjects-container-outer',
+  --           ['i;'] = { 'textsubjects-container-inner', desc = 'Select inside containers' },
+  --         },
+  --       },
+  --     }
+  --   end,
+  -- },
   ------------------------------------------------------------------------------
   -- https://github.com/toppair/peek.nvim
   -- Markdown preview:
@@ -1680,20 +1575,18 @@ return {
   },
   {
     'tris203/precognition.nvim',
+    event = 'VeryLazy', -- Load at startup, not deferred by keys
     opts = {
       startVisible = true,
       showBlankVirtLine = false,
       highlightColor = { fg = '#1abc9c', bg = '#1f2335' }, -- teal fg + darker than cursorline
     },
-    keys = {
-      {
-        '<leader>tp',
-        function()
-          require('precognition').toggle()
-        end,
-        desc = 'Toggle Precognition hints',
-      },
-    },
+    config = function(_, opts)
+      require('precognition').setup(opts)
+      vim.keymap.set('n', '<leader>tp', function()
+        require('precognition').toggle()
+      end, { desc = 'Toggle Precognition hints' })
+    end,
   },
 
   -------------------------------------------------------------------------------
